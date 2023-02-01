276c4e7c695b0bd8765914936bf104888108fef1
JonnyDavison <jonnydavison@icloud.com>
2023-02-01 09:18:57 +0000

    Set up JS function structure of game

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAwnser(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
