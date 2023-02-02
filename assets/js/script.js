cb9de8b682513f319102abce36ef15eef01704b5
JonnyDavison <jonnydavison@icloud.com>
2023-02-02 13:28:25 +0000

    Compleate &  correct division function to use integers

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,7 +82,7 @@ function calculateCorrectAnswer(){
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [operand1 / operand2, "divison"];
+        return [operand1 / operand2, "division"];
     } else{
         alert(`Unimplimented operator ${operator}`);
         throw `Unimplimented operator ${operator}. Aborting!`; 
@@ -125,7 +125,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;   
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 * operand2    
+    document.getElementById('operand2').textContent = operand2; 
     document.getElementById('operator').textContent = '/';
 }
